516b60884c1e0b42277b75628938a5eb65523627
Ankit Meena <63893740+ankitmeena007@users.noreply.github.com>
2024-01-25 19:48:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public/"));
 
 mongoose.connect("mongodb+srv://ankitmeena007:kaka@jiji@fc7e9d21@cluster0.v5i8l.mongodb.net/todolistDB", {useNewUrlParser: true});
 
